6bc20331ce17ae06fa5a27b63726e2fdcf7d06c0
unknown <shakirck333@gmail.com>
2020-06-06 12:41:35 +0545

    updated snakes json

---
+++ static/js/rendersnakes.js
@@ -0,0 +1,62 @@
+//venomous snakes
+
+var venomousContainer = document.getElementById('venom-snakes')
+var nonVenomousContainer = document.getElementById('non-venom-snakes')
+var venomousSnakes = snakesJSON.venomous;
+var nonVenomousSnakes = snakesJSON.nonVenomous;
+var card ;
+
+
+
+
+
+function renderVenomousSnakes(){
+    for( snake of venomousSnakes){
+        var li = document.createElement('li');
+        li.classList.add('snake-item');
+        card = `<!-- Card Starts  -->
+        <div class="card snake-card" style="width: 18rem;">
+        <img src="${snake.url}" class="card-img-top"
+            alt="">
+        <div class="card-body">
+            <h5 class="card-title">${snake.name}</h5>
+            <p class="card-text"> ${snake.malayalam} </p>
+            <div class="btns">
+            <div class="btn-danger btn">Highly Venomous</div>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a>-->
+            </div>
+        </div>
+        </div>
+<!-- Card Ends -->`
+li.innerHTML=card;
+venomousContainer.appendChild(li);
+    }
+
+}
+
+function renderNonVenomousSnakes(){
+    for( snake of nonVenomousSnakes){
+        var li = document.createElement('li');
+        card = `<!-- Card Starts  -->
+        <div class="card snake-card" style="width: 18rem;">
+        <img src="${snake.url}" class="card-img-top"
+            alt="">
+        <div class="card-body">
+            <h5 class="card-title">${snake.name}</h5>
+            <p class="card-text"> ${snake.malayalam} </p>
+            <div class="btns">
+            <div class="btn-success btn">Non Venomous</div>
+           <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
+        </div>
+<!-- Card Ends -->`
+li.innerHTML=card;
+nonVenomousContainer.appendChild(li);
+    }
+
+}
+
+
+renderVenomousSnakes();
+renderNonVenomousSnakes();
---
+++ static/js/snakes.js
@@ -0,0 +1,78 @@
+var snakesJSON = {
+"venomous":[
+    {
+        "name": "hump-nosed pit viper (Hypnale hypnale)"         ,
+        "malayalam" : "ചുരുട്ട ",
+        "url":"http://13.126.210.153/u/611c0845-32b4-43fe-9454-73a6c7955872.jpg",
+        "distribution":"Found only in the western ghats",
+        "description":`A Nocturnal and terrestrial pitviper, found in evergreen to dry deciduous forest and plantations in the region. Head is flat, triangular and broader than neck with a hump on the tip of snout. eyes have vertical pupil. Heat sensing pit present between eyes and nostril.s Sides of head usually dark brown in color, edged above with a whitish line ; sometimes a white, black-edged, longitudinal streak on each side of the neck. Scales are smooth or weakly keeled.Body color varying from pale grey to reddish brown with black specks, patches or spots on the body.
+        Prefer moist and leaf littered ground or found under rocks, logs with its head angled slightly upwards. Excellently camouflaged and stay motionless on the ground as an ambush predator. Feeds on small rodents, lizards, geckos and frogs.
+        Bites need medical attention` ,
+        "states":" Karnataka, Kerala, Tamil Nadu, Goa, Maharashtra", 
+    },
+
+    {
+        "name":       "king cobra" ,
+        "malayalam":       "രാജ  വെമ്പാല " ,
+        "url":"http://13.126.210.153/u/d84b1f5c-c04f-4f33-9bc0-07841fd143cc.jpg",
+        "distribution":     " Found in low to moderate elevation up to 2005m. ",
+        "description":     `New born- 43-50cm., average- 300cm (10ft)., maximum- 585cm (19.19ft). Individuals above 480cm (16ft) are not common in India.
+
+        Head large with rounded snout, covered with large shields and slightly broader than neck. Large eyes bear rounded pupil.Body very long, slender and covered with large smooth scales. Dorsal color variable according to geography which includes grey, black, dark olive green, yellow-brown with yellow or white bands throughout the body. Population of far north-east have almost patternless appearance of bluish-black. Population of Andaman Islands is rusty-brown with or without bands. Bands of hood region are inverted V shaped and known for having characteristic to differentiate individuals. Ventral color usually pale yellow or grayish with dark shades on edge of many ventral scales. Underside of hood bears extensions of dorsal coloration which is characteristic to recognize different individuals.`     ,
+        "states": 'Andhra Pradesh, Arunachal Pradesh, Assam, Chhattisgarh, Goa, Karnataka, Kerala, Manipur, Meghalaya, Mizoram, Nagaland, Odisha, Sikkim, Tamil Nadu, Tripura, Uttarakhand, West Bengal, Andaman and Nicobar, '
+    
+    },
+
+    
+    {
+        "name": "spectacled cobra (Naja naja)"  ,
+        "malayalam": "മൂർഖൻ " ,
+        "url":"http://13.126.210.153/u/88b1a909-be0b-41a7-8384-05ec07d9fa55.jpg",
+        "distribution":`Head triangular with rounded edge, not broader than neck. The famous hood mark appears when stretched on provocation. In dark color adult specimens from North India hood mark becomes faint or absent in adults. Eyes have rounded pupil.
+
+        Body slender with smooth oval shaped scales. Dorsal color geographically variable from yellow, all shades of brown, dark gray, reddish, black or black mixed with blue, purple, red etc. Side dorsal scales larger and clearly oval shaped while scales on the top narrower and become pointed. Maharashtra, whole South India, Andhra Pradesh, Odisha, West Bengal etc. coastal states populations lighter while central-northern population dark brown or black. Ventral color ranges from milky white, yellowish, white mixed with purple, red, brown, gray etc with or without dark patches.`      ,
+        "description":   `Head triangular with rounded edge, not broader than neck. The famous hood mark appears when stretched on provocation. In dark color adult specimens from North India hood mark becomes faint or absent in adults. Eyes have rounded pupil.
+
+        Body slender with smooth oval shaped scales. Dorsal color geographically variable from yellow, all shades of brown, dark gray, reddish, black or black mixed with blue, purple, red etc. Side dorsal scales larger and clearly oval shaped while scales on the top narrower and become pointed. Maharashtra, whole South India, Andhra Pradesh, Odisha, West Bengal etc. coastal states populations lighter while central-northern population dark brown or black. Ventral color ranges from milky white, yellowish, white mixed with purple, red, brown, gray etc with or without dark patches.`       ,
+        "states" :"Goa, Gujarat, Haryana, Jharkhand, Karnataka, Kerala, Madhya Pradesh, Maharashtra, Odisha, Punjab, Rajasthan, Tamil Nadu, Telangana, Uttar Pradesh, Uttarakhand, Chandigarh, Dadra and Nagar Haveli, Daman and Diu, Delhi, Puducherry, "
+    
+    }
+
+
+    // {
+    // "name":                 ,
+    // "malayalam":            ,
+    // "url":
+    // "distribution":      ,
+    // "description":          ,
+    // "states"
+
+    // }
+],
+"nonVenomous":[
+   {
+    "name":"travancore kukri snake"            ,
+    "malayalam":  "കുക്രി പാമ്പ് | പൂച്ചക്കണ്ണൻ പാമ്പ് "          ,
+    "url":"http://13.126.210.153/u/aa9b792e-039b-45b4-9038-b129fdb55d9d.jpg",
+    "distribution": "Found in south of Palghat Gap of Kerala and Tamil Nadu. Type locality: South Travancore Hills, Kerala"     ,
+    "description":  `Can grow upto 51cm.
+
+    Head small, indistinct from neck. Top of the head with black color chevron shaped marking from prefrontals to end of it which usually remain connected with a narrow streak between eyes. Parietals usually have one-one closely placed whitish spots. Small eyes have rounded pupil.
+    
+    Body short, moderately stout and covered with glossy smooth scales. Dorsal ground color grayish-brown with blackish-brown bands margined with white, broader on top and tapers on flank. Between conspicuous bands very faint bands also present throughout.
+    
+    Underside including ventrals and subcaudals densely marked with irregularly arranged rectangular black patches on yellow-green ground color.`       ,
+    "states":"Kerala, Tamil Nadu, Karnataka, "
+
+    },
+    {
+    "name":      "striped keelback "           ,
+    "malayalam":        "തെയ്യം പാമ്പ് "    ,
+    "url":"http://13.126.210.153/u/1b4a9392-f219-43a8-afe2-2111c6899d2b.jpg",
+    "distribution":  "Found in whole of Indian mainland including North and North-east states. Not found in Islands. "    ,
+    "description":  `Head pointed with smooth scales, broader than neck. Olive brown on upper and overall yellowish including underside and neck. Upper lip marked with blackish edge on their posterior edge.Body slender with pointed and highly keeled scales. Brownish back with continuous black bands which fuse to form blackish-brown posterior body. Two yellow-brown stripes run longitudinally from neck to end of the tail where they become prominent; these lines become very faint yellow when intersect with blackish bands. Interscale color blue in population of most of the parts of country (called forma typica variety) while individuals from South India, Maharastra and some parts of North-east specimens bear red interscale color (called erythrostictus variety). These blue and red color appears clearly on provoking the snake.Ventral glossy white with reddish or grayish ting on side near dorsals; blackish dots may or may not be present on each side of ventrals.`        ,
+    "states": "Andhra Pradesh, Arunachal Pradesh, Assam, Bihar, Chhattisgarh, Goa, Gujarat, Haryana, Himachal Pradesh, Jammu and Kashmir, Jharkhand, Karnataka, Kerala, Madhya Pradesh, Maharashtra, Manipur, Meghalaya, Mizoram, Nagaland, Odisha, Punjab, Rajasthan, Sikkim, Tamil Nadu, Telangana, Tripura, Uttar Pradesh, Uttarakhand, West Bengal, Chandigarh, Delhi, Puducherry, Daman and Diu, "
+
+    }
+]
+}
