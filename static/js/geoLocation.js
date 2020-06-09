d8051be9594876cd49dfc6a140cb4e190d340289
unknown <shakirck333@gmail.com>
2020-06-09 23:20:50 +0545

    debug

--- static/js/geoLocation.js
+++ static/js/geoLocation.js
@@ -4,7 +4,7 @@
 var x = document.getElementById("demo");
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition,function(){console.log('error geolocation')},{enableHighAccuracy:true, timeout:60000, maximumAge:600000});
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
